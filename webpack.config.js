4f9903c5a30e49d248192e666a6bb5887d710269
HyeongJun Yoon <family72020@gmail.com>
2023-10-31 00:21:43 +0900

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  mode: "production",
+  // 모드 production시 (default) devtool: "source-map",
+  //devtool: false,
+  entry: path.resolve("src/app.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve("./"),
+  },
+};
