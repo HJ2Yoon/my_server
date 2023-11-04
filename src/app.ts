4ab9908b350e9233b6176c5ff534c4276d9de7e6
HyeongJun Yoon <family72020@gmail.com>
2023-11-04 17:43:39 +0900

    test #4

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,91 @@
 import express, { Request, Response } from "express";
-import AWS from "aws-sdk";
 import { getClientIp } from "request-ip";
 import cors from "cors";
+import { docClient, twitchAuth } from "./aws";
+import { getTwitchUsers } from "./api";
 
+//#region Server initial
 const app = express();
 const port = 3000;
+
 const clients = new Map<string, Response>();
-const docClient = new AWS.DynamoDB.DocumentClient({ region: "ap-northeast-2" });
+const streamers = new Map<string, { [key: string]: string }>();
+const headerParams = new Map<string, string>();
+
+twitchAuth().then(({ token, auth }) => {
+  if (!token || !auth) return;
+  headerParams.set("clientId", auth[0]);
+  headerParams.set("accessToken", token[0].access_token);
+});
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-// const whiteList: string[] = [];
+//#endregion
 
 app.use(cors());
 
 app.get("/getStream", async (req: Request, res: Response) => {
   try {
-    const users = (await docClient.scan({ TableName: "Streamer" }).promise())
+    const items = (await docClient.scan({ TableName: "Streamer" }).promise())
       .Items;
-    res.send(JSON.stringify(users));
+    items?.forEach((item) => {
+      streamers.set(item.display_name, item);
+    });
+    res.send(streamers);
   } catch (err) {
     console.log(err);
   }
+});
+
+app.get("/putStream", async (req: Request, res: Response) => {
+  /*const message = req.query.message as string;
+  if (!streamers.has(message)) {
+    // Search users in twitch
+
+    // Update on "Streamers" DynamoDB
+    let updateExpression = "SET ";
+    const expressionAttributeKeys: { [key: string]: string } = {};
+    const expressionAttributeValues: { [key: string]: any } = {};
+
+    const attributes = {};
+
+    // attributes 객체키를 배열로 나열하고 이를 순회
+    Object.keys(attributes).forEach((key, index) => {
+      const placeholder = `#attr${index}`;
+      updateExpression += `${placeholder} = :value${index}, `;
+      expressionAttributeKeys[placeholder] = key;
+      expressionAttributeValues[`:value${index}`] = attributes[key];
+    });
+
+    const params = {
+      TableName: "Streamer",
+      Key: {
+        streamer_id: message,
+      },
+      UpdateExpression: updateExpression.slice(0, -2),
+      ExpressionAttributeNames: expressionAttributeKeys,
+      ExpressionAttributeValues: expressionAttributeValues,
+    };
+
+    docClient.update(params);
+  }*/
 });
 
 app.get("/report", (req: Request, res: Response) => {
   res.send(`Current clients: ${clients.size}`);
 });
 
-app.get("/change", (req: Request, res: Response) => {
+app.get("/change", async (req: Request, res: Response) => {
   const message = req.query.message as string;
-  sendEvent({ message });
-  res.end();
+
+  res.send(
+    await getTwitchUsers(
+      message,
+      headerParams.get("clientId") as string,
+      headerParams.get("accessToken") as string
+    )
+  );
 });
 
 app.get("/sse", (req: Request, res: Response) => {
